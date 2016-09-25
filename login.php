f29b46036db9fcb181e229dc5af18baec7b80451
Jamshid <mr.jamshid.a@gmail.com>
2016-09-25 22:47:53 +0330

    completed html for login page

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<div class="login">
+		<form action="" method="post">
+			<input type="text" name="username" placeholder="User Name">
+			<input type="password" name="password" placeholder="******">
+			<input type="submit" name="login" value="Log in">
+		</form>
+	</div>
+
+</body>
+</html>
