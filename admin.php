355ffaac00287e1900b48559ba4be32e1ddbd96d
Jamshid <mr.jamshid.a@gmail.com>
2016-09-26 14:21:13 +0330

    added categories file

--- admin.php
+++ admin.php
@@ -18,6 +18,14 @@ if(!isset($_SESSION['admin_logged_in'])) {
 
 <div class="admin">
 	<h2>Admin center</h2>
+	<div class="top-menu">
+		<ul>
+			<li><a href="#">Main page</a></li>
+			<li><a href="#">Posts</a></li>
+			<li><a href="cats.php">Categories</a></li>
+			<li><a href="#">Log out</a></li>
+		</ul>
+	</div>
 </div>	
 
 
---
+++ cats.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html> 
+<head>
+	<title>Categories</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="admin">
+	<h2>Admin center</h2>
+	<div class="top-menu">
+		<ul>
+			<li><a href="admin.php">Main page</a></li>
+			<li><a href="#">Posts</a></li>
+			<li><a href="#">Categories</a></li>
+			<li><a href="#">Log out</a></li>
+		</ul>
+	</div>
+	<div class="add-cat">
+		<form action="" method="post">
+			<input type="text" name="cat-name" placeholder="new category name">
+			<input type="submit" value="Add new category">
+		</form>
+	</div>
+</div>	
+
+
+</body>
+</html>
