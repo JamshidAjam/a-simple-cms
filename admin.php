5af38d45df5860f50584cdf68a8a359c0c79f5fc
Jamshid <mr.jamshid.a@gmail.com>
2016-09-26 13:53:39 +0330

    corrected login access

--- admin.php
+++ admin.php
@@ -2,10 +2,24 @@
 
 session_start();
 
-if(isset($_SESSION['admin_logged_in'])) {
-	echo "session was created.";
-} else {
-	echo "there is no session!";
+if(!isset($_SESSION['admin_logged_in'])) {
+	header('Location: login.php');
 }
 
 ?>
+
+<!DOCTYPE html>
+<html> 
+<head>
+	<title>Management Center</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="admin">
+	<h2>Admin center</h2>
+</div>	
+
+
+</body>
+</html>
